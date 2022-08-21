0845ecd6ef2ae74ee0d6ecbe2d75f7f908b2d519
sabbirzzaman <sztonmoy7@gmail.com>
2022-08-21 20:35:20 +0600

    UI added

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../images/lws.svg'
+
+const Header = () => {
+    return (
+        <nav className="bg-slate-100 shadow-md">
+            <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3 items-center">
+                <a href="index.html">
+                    <img
+                        className="h-10"
+                        src={Logo}
+                        alt="Learn with Sumit"
+                    />
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
